143b71cd3b75438da2aac4ed9a85d74e62292488
admiral <kartikeyk1776@gmail.com>
2024-12-13 00:30:00 +0530

    add hlo.cpp

---
+++ hlo.cpp
@@ -0,0 +1,34 @@
+#include <iostream>     // input output stream
+// # - preprocessor directives
+/*preprocessor is a tool that runs before the actual compilation of your C++ code.
+    Its job is to process special instructions in your program (called preprocessor directives)
+    and prepare the source code for the compiler.
+*/
+
+using namespace std;    //  tells the compiler to use standard namespace by default (in entire code)
+
+
+// can also use specific element of the namespace:
+// using std::cout;  
+// using std::endl;
+
+// create own namespace
+
+// namespace myKwa{
+//     void dis(){
+//         cout << "bruh";
+//     }
+// }
+
+int main(){
+    // whenever we give exe file to the OS the main method is the method from which OS starts executing the file
+    cout << "hlo wassup bye bye kk \n";     // << means passing on a val to cout
+    cout << "wow" << endl;  // endl -> (std::endl) -> end line
+
+    //myKwa::dis();    // using custom namespace
+}
+// if main func returns 0, means there was no err and code ran successfully
+
+int halo(){
+
+}
