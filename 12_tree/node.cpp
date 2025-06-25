b64bcaed92866c7017c07f50ad9b387b986f0055
admiral <kartikeyk1776@gmail.com>
2025-06-25 18:07:56 +0530

    nodes

--- 12_tree/node.cpp
+++ 12_tree/node.cpp
@@ -5,31 +5,43 @@ using namespace std;
 class Node{
     public:
         int data;
-        Node* left = nullptr;
-        Node* right = nullptr;
+        Node* left;
+        Node* right;
 
     Node(int value){
         data = value;
+        right = nullptr;
+        left = nullptr;
     }
 
-    void dataVal(){
-        cout << data << "\n" ;
-    }
+    // void dataVal(){
+    //     cout << data << "\n" ;
+    // }
 
 };
 
 
 int main(){
 
-    Node value = Node(5);
+    // Node value = Node(5);
 
-    value.dataVal();
+    // value.dataVal();
 
-    Node value2 = Node(7);
+    // Node value2 = Node(7);
 
-    value.left = &value2;
+    // value.left = &value2;
 
-    value.left->dataVal();
+    // value.left->dataVal();
+
+    Node* firstNode = new Node(4);
+    Node* secondNode = new Node(5);
+    Node* thirdNode = new Node(6);
+    Node* fourthNode = new Node(7);
+
+    firstNode->left = secondNode;
+    firstNode->right = thirdNode;
+    secondNode->left = fourthNode;
+
 
     return 0;
 
